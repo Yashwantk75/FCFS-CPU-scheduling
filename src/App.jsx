47f30a4e937dc8d0cc30acf09a65ba16e415b5cf
Yashwantk75 <yashwantkk4444@gmail.com>
2024-10-16 22:20:21 +0530

    version2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Chart from 'react-google-charts';
+import styled from 'styled-components';
+import img from './assets/queue.jpg';
+import { useNavigate } from 'react-router-dom';
 function App() {
+  const navigate = useNavigate();
 
   return (
-    <Chart
-      chartType="Timeline"
-      data={[
-        [
-          { type: 'string', label: 'Process' },
-          { type: 'string', label: 'Label' },
-          { type: 'date', label: 'Start' },
-          { type: 'date', label: 'End' }
-        ],
-        [
-          "President",
-          "George Washington",
-          0,
-          1000,
-        ],
-      ]}
-      width="100%"
-      height="400px"
-    />
+    <Container>
+      <div className="card">
+        <img className="card-img-top" src={img} alt="Card image cap" />
+        <div className='image'></div>
+        <div className="card-body">
+          <h5 className="card-title">FCFS</h5>
+          <div className="card-text">The First-Come, First-Served (FCFS) CPU scheduling algorithm processes tasks in the order they arrive. It is non-preemptive, meaning once a process starts executing, it runs until completion. While simple to implement, it may lead to longer waiting times if a lengthy process arrives before shorter ones other process has to wait.</div>
+          <button onClick={() => navigate('/fcfs')} className='btn btn-primary'>Demo</button>
+        </div>
+      </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  .card{
+    width: 200px;
+    height: auto;
+    img{
+      height:  200px;
+      width: 200px;
+    }
+    .card-body{
+      .card-text{
+        font-size: 8px;
+      }
+      button{
+        font-size: 12px;
+        margin-top: 5px;
+      }
+    }
+  }
+`;
+
 
 export default App
